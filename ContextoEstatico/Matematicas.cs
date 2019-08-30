e1ad1c45bd1413492f2934bf6601e3d5d7b4481f
alumno <alumno@E303>
2019-08-30 08:42:40 -0700

    Agregar archivos de proyecto.

---
+++ ContextoEstatico/Matematicas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextoEstatico
+{
+    class Matematicas
+    {
+        public const double PI = 3.1459;  
+        public static double sumar(double sumando1, 
+            double sumando2)
+        {
+            return sumando1 + sumando2;
+        }
+
+        public static double areaCirculo(double radio)
+        {
+            return (radio * radio) * PI;
+        }
+    }
+}
+
+//const // la va dejar estatica y dejar cambiar el valor
+// static // el valor es estatico
